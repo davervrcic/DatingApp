f840b3f8609b8fe12a80ae34d0db7ee9be14084f
Daver <daver.vrcic@gmail.com>
2020-04-02 23:01:11 +0200

    End of section 3

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify pass 4-8 caracters")]
+
+        public string Password { get; set; }
+    }
+}
