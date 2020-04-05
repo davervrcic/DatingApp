8b9016173b633386eae23bb99e67da375efbd064
Daver <daver.vrcic@gmail.com>
2020-04-05 13:04:30 +0200

    End of section 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -49,6 +49,7 @@ namespace DatingApp.API.Controllers
 
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+                        
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
@@ -78,7 +79,8 @@ namespace DatingApp.API.Controllers
             return Ok(new{
                 token = tokenHandler.WriteToken(token)
             });
-        }
 
+            }
+          
     }
 }
