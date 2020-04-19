7032eccf431a679e8b09aef2b6b69013deaa8ffc
Daver <daver.vrcic@gmail.com>
2020-04-19 15:23:42 +0200

    en of section 11

--- DatingApp.API/Dtos/PhotoForReturnDto.cs
+++ DatingApp.API/Dtos/PhotoForReturnDto.cs
@@ -1,10 +1,10 @@
 using System;
 
-namespace DatingApp.API.Models
+namespace DatingApp.API.Dtos
 {
-    public class Photo
+    public class PhotoForReturnDto
     {
-        public int Id { get; set; }
+         public int Id { get; set; }
         public string Url { get; set; }
 
         public string Description { get; set; }
@@ -14,9 +14,5 @@ namespace DatingApp.API.Models
         public bool IsMain { get; set; }
 
         public string PublicId { get; set; }
-
-        public User User { get; set; }
-
-        public int UserId  { get; set; }
     }
 }
